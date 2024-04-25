ca361f27463a59519a79230a4de4b61c1771f4ef
shahzad akram <alyakram786@gmail.com>
2024-04-26 02:39:14 +0500

    adding project to repo

---
+++ src/Components/Body/CardsArray/cardArray.js
@@ -0,0 +1,24 @@
+
+
+
+const CardArray = [
+    {
+        text:"Sheraz",
+        title:"Ceo",
+        image:"image"
+    },
+    {
+        text:"Shahzad",
+        title:"Front E developer",
+        image:"image"
+    },
+    {
+        text:"Shahzain",
+        title:"Back E developer",
+        image:"image"   
+}
+]
+
+
+
+export default CardArray
---
+++ src/page/home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Body from '../../Components/Body/Body'
+
+export default function Home() {
+  return (
+    <Body/>
+    
+  )
+}
